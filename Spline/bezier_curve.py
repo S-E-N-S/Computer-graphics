a351fbe7913128c9fcd691d3f22c665719fe3992
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2021-02-26 21:59:33 +0300

    Minor refactor
    Refactor BezierManager

--- Spline/bezier_curve.py
+++ Spline/bezier_curve.py
@@ -2,31 +2,27 @@ import math
 import numpy as np
 
 
-def findPoint(points, t):
-    n = len(points)
-    point = np.array([0, 0])
-    for i in n:
-        point += math.comb(i, n) * t**i * (1 - t)**(n-i) * points[i]
-    return point
-
-class Bezier_manager:
-    
+class BezierManager:
     def __init__(self, n=3):
         self.n = n
         self.points = np.array([[0, 10], [50, 100], [100, 30]])
 
-
-    def set_points(new_points):
+    def set_points(self, new_points):
         self.points = new_points
         self.n = len(new_points)
     
-    
     def get_points(self, t):
-        extra_points = [points]
-        for i in range(n-2):
+        extra_points = [self.points]
+        for i in range(self.n - 2):
             level_points = []
             for j in range(len(extra_points[i]) - 1):
                 level_points += t * extra_points[i][j] + (1 - t) * extra_points[i][j + 1]
             extra_points += level_points
-        extra_points += [findPoint(points, t)]
+        extra_points += [self.find_point(t)]
         return extra_points
+
+    def find_point(self, t):
+        point = np.array([0, 0])
+        for i in range(self.n):
+            point += math.comb(i, self.n) * t ** i * (1 - t) ** (self.n - i) * self.points[i]
+        return point
