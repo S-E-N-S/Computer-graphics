11d90036c8fe566f84f4d91a6a60ab18e556daec
NikitaS4 <nikita-s4astlivtsev2012@yandex.ru>
2021-02-26 20:31:38 +0300

    Add main window
    Add main window

--- Spline/main.py
+++ Spline/main.py
@@ -1,5 +1,12 @@
+from PySide2.QtWidgets import QApplication, QMainWindow
+
+
 def entry_point():
-    pass
+    app = QApplication()
+    window = QMainWindow()
+    window.setWindowTitle("Bezier Splines by S-E-N-S")
+    window.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
