cd884b60b0dba6718518e15e953964b8728404c7
MethaHardworker <karasevalexander1999@gmail.com>
2021-02-26 21:55:51 +0300

    set points

--- Spline/bezier_curve.py
+++ Spline/bezier_curve.py
@@ -14,10 +14,16 @@ class Bezier_manager:
     def __init__(self, n=3):
         self.n = n
         self.points = np.array([[0, 10], [50, 100], [100, 30]])
-        
+
+
+    def set_points(new_points):
+        self.points = new_points
+        self.n = len(new_points)
+    
+    
     def get_points(self, t):
         extra_points = [points]
-        for i in range(n):
+        for i in range(n-2):
             level_points = []
             for j in range(len(extra_points[i]) - 1):
                 level_points += t * extra_points[i][j] + (1 - t) * extra_points[i][j + 1]
