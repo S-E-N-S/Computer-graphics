Fix formula
Fix formula

rt.py
@@ -29,16 +29,6 @@ MAX_DEPTH_RAY_TRACING = 5  # how many times we can split a single ray
 RGB_DIM = 3  # how many components are needed to determine color
 NORM_BIAS_SCALE = 0.0001  # displacement for the normal vector 
 
-
-# List of objects.
-color_plane0 = 1.0 * np.ones(RGB_DIM)
-color_plane1 = 0.0 * np.ones(RGB_DIM)
-scene = [add_sphere([0.75, 0.1, 1.0], 0.6, [0.0, 0.0, 1.0]),
-         add_transparent_sphere([-0.75, 0.1, 2.25], 0.6, [0.5, 0.223, 0.5], 0.8),
-         add_sphere([-2.75, 0.1, 3.5], 0.6, [1.0, 0.572, 0.184]),
-         add_plane([0.0, -0.5, 0.0], [0.0, 1.0, 0.0]),
-   ]
-
 # Light position and color.
 L = np.array([5.0, 5.0, -10.0])
 color_light = np.ones(3)
@@ -88,9 +78,8 @@ def intersect_sphere(O, D, S, R):
     disc = b * b - 4 * a * c
     if disc > 0:
         distSqrt = np.sqrt(disc)
-        q = (-b - distSqrt) / 2.0 if b < 0 else (-b + distSqrt) / 2.0
-        t0 = q / a
-        t1 = c / q
+        t0 = (-b - distSqrt) / 2.0 / a
+        t1 = (-b + distSqrt) / 2.0 / a
         t0, t1 = min(t0, t1), max(t0, t1)
         if t1 >= 0:
             return t1 if t0 < 0 else t0
@@ -143,6 +132,7 @@ def trace_ray(rayO, rayD):
     M = rayO + rayD * t
     # Find properties of the object.
     N = get_normal(obj, M)
+    #N *= 1 if np.dot(rayD, N) <= 0 else -1
     color = get_color(obj, M)
     toL = normalize(L - M)
     toO = normalize(O - M)
@@ -180,16 +170,19 @@ def add_plane(position, normal):
 
 def find_refracted(N, M, rayO, rayD, refraction):
     # ray goes in/out object
-    N *= 1 if np.dot(rayD, N) <= 0 else -1
-    rayO_refracted = M - N * NORM_BIAS_SCALE
+    normal = N.copy()
+    if np.dot(rayD, N) >= 0:
+        normal *= -1
+        refraction = 1 / refraction
+    rayO_refracted = M - normal * NORM_BIAS_SCALE
     # find sin (sin^2 + cos^2 = 1)
     # cos is dot product
-    fall_sin = (1 - np.dot(rayD, N) ** 2) ** 0.5
-    refraction_sin = fall_sin * refraction
+    fall_sin = (1 - np.dot(rayD, normal) ** 2) ** 0.5
+    refraction_sin = fall_sin / refraction
     refraction_cos = (1 - refraction_sin ** 2) ** 0.5
-    refraction_cot = refraction_cos / refraction_sin
-    surf_dir = N * np.dot(rayD, N) + rayD
-    rayD_refracted = normalize(N + surf_dir * refraction_cot)
+    refraction_tg =  refraction_sin / refraction_cos
+    surf_dir = normal * np.dot(rayD, normal) + rayD
+    rayD_refracted = normalize(-normal + surf_dir * refraction_tg)
     return rayO_refracted, rayD_refracted
 
 
@@ -220,20 +213,39 @@ def cast_ray(rayO, rayD, reflection, depth=0):
     ray_sum += cast_ray(rayO, rayD, reflection, depth + 1)
     return ray_sum + reflection * col_ray
 
-# Loop through all pixels.
-for i, x in enumerate(np.linspace(S[0], S[2], w)):
-    if i % 10 == 0:
-        print(i / float(w) * 100, "%")
-    for j, y in enumerate(np.linspace(S[1], S[3], h)):
-        #print(f"{j, y}")
-        col[:] = 0
-        Q[:2] = (x, y)
-        D = normalize(Q - O)
-        rayO, rayD = O, D
-        reflection = 1.
-        # Loop through initial and secondary rays.
-        col = cast_ray(rayO, rayD, reflection)
-        img[h - j - 1, i, :] = np.clip(col, 0, 1)
 
-# save the result
-plt.imsave('fig.png', img)
+# List of objects.
+color_plane0 = 1.0 * np.ones(RGB_DIM)
+color_plane1 = 0.0 * np.ones(RGB_DIM)
+scene = [add_sphere([0.75, 0.1, 1.0], 0.6, [0.0, 0.0, 1.0]),
+         add_transparent_sphere(position=[-0.75, 0.1, 2.25], radius=0.6, 
+            color=[0.5, 0.223, 0.5], refraction=1),
+         add_sphere(position=[-0.75, 0.1, 5.0], radius=0.7, color=[0.8, 0.2, 0.0]),
+         add_sphere([-2.75, 0.1, 3.5], 0.6, [1.0, 0.572, 0.184]),
+         add_plane([0.0, -0.5, 0.0], [0.0, 1.0, 0.0]),
+        ]
+
+
+def main():
+    global col
+    # Loop through all pixels.
+    for i, x in enumerate(np.linspace(S[0], S[2], w)):
+        if i % 10 == 0:
+            print(int(i / float(w) * 100), "%")
+        for j, y in enumerate(np.linspace(S[1], S[3], h)):
+            #print(f"{j, y}")
+            col[:] = 0
+            Q[:2] = (x, y)
+            D = normalize(Q - O)
+            rayO, rayD = O, D
+            reflection = 1.
+            # Loop through initial and secondary rays.
+            col = cast_ray(rayO, rayD, reflection)
+            img[h - j - 1, i, :] = np.clip(col, 0, 1)
+
+    # save the result
+    plt.imsave('fig.png', img)
+
+
+if __name__ == "__main__":
+    main()